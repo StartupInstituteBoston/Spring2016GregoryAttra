53ce1a1d9dff7832db109911f23a045b85ae8d1e
Broderick Attra <barbaraharvey@Barbaras-MacBook-Air.local>
2016-03-18 15:38:47 -0400

    regx validation for phone number

--- restaurant_mar16/app/models/restaurant.rb
+++ restaurant_mar16/app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ActiveRecord::Base
+	validates_presence_of :name, :description, :owner_id
+  validates(:phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "invalid phone number" })
 end
