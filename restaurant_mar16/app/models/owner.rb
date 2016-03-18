ea5cee7c9627b586010e14238fdff4df901040d5
Broderick Attra <barbaraharvey@Barbaras-MacBook-Air.local>
2016-03-18 15:49:16 -0400

    stupid bug shit

--- restaurant_mar16/app/models/owner.rb
+++ restaurant_mar16/app/models/owner.rb
@@ -3,7 +3,9 @@ class Owner < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   has_many :restaurants
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+         
 end
--- restaurant_mar16/app/models/restaurant.rb
+++ restaurant_mar16/app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ActiveRecord::Base
+	belongs_to :owner
 	validates_presence_of :name, :description, :owner_id
   validates(:phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "invalid phone number" })
 end
