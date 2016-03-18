9e470ed38b0dbe6de8e54d69e21c8c6757219f95
Broderick Attra <barbaraharvey@Barbaras-MacBook-Air.local>
2016-03-18 12:57:43 -0400

    now a user may view the restaurants page without being logged in

--- restaurant_mar16/app/controllers/restaurants_controller.rb
+++ restaurant_mar16/app/controllers/restaurants_controller.rb
@@ -1,44 +1,44 @@
 class RestaurantsController < ApplicationController
-  before_filter :authenticate_owner!
-  	def index 
-  		@restaurants = Restaurant.all
-  	end
+  #before_filter :authenticate_owner!
+	def index 
+		@restaurants = Restaurant.all
+	end
 
-  	def new
-  		@restaurant = current_owner.restaurants.build
-      render layout: false if request.xhr?
-  	end
+	def new
+		@restaurant = current_owner.restaurants.build
+    render layout: false if request.xhr?
+	end
 
-    def create
-      @restaurant = current_owner.restaurants.build(restaurant_params)
-  		@restaurant.save
-  		redirect_to restaurants_url
-  	end
+  def create
+    @restaurant = current_owner.restaurants.build(restaurant_params)
+		@restaurant.save
+		redirect_to restaurants_url
+	end
 
-  	def show
-  		@restaurant = Restaurants.find(params[:id])
-      render layout: false if request.xhr?
-  	end
+	def show
+		@restaurant = Restaurants.find(params[:id])
+    render layout: false if request.xhr?
+	end
 
-    def edit 
-      @restaurant = current_owner.restaurants.find(params[:id])
-      render layout: false if request.xhr?
-    end
+  def edit 
+    @restaurant = current_owner.restaurants.find(params[:id])
+    render layout: false if request.xhr?
+  end
 
-  	def update
-  		@restaurant = current_owner.restaurants.find(params[:id])
-  		@restaurant.update_attributes(restaurant_params)
-  		redirect_to restaurants_url
-  	end
+	def update
+		@restaurant = current_owner.restaurants.find(params[:id])
+		@restaurant.update_attributes(restaurant_params)
+		redirect_to restaurants_url
+	end
 
-  	def destroy
-  		@restaurant = current_owner.restaurants.find(params[:id])
-  		@restaurant.destroy
-      redirect_to restaurants_url
-  	end
+	def destroy
+		@restaurant = current_owner.restaurants.find(params[:id])
+		@restaurant.destroy
+    redirect_to restaurants_url
+	end
 
-  	private 
-    def restaurant_params
-			params.require(:restaurant).permit(:id, :address, :city, :state, :zipcode, :description, :name, :phone)	
-  	end
+	private 
+  def restaurant_params
+		params.require(:restaurant).permit(:id, :address, :city, :state, :zipcode, :description, :name, :phone)	
+	end
 end
